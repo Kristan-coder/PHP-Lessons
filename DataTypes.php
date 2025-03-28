b66a2a90ca8abc467d2042743514c9421a60cb97
Kristan-coder <141744270+Kristan-coder@users.noreply.github.com>
2025-03-28 09:37:51 -0500

    Create DataTypes.php

---
+++ DataTypes.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php
+    //string
+     $name = 'Coding is the best class in Madison Academic Magnet Highschool';
+
+    //Integer
+    $name = 20;
+
+    //Float
+    $name = 65.4184;
+
+    //BooLean
+   
+
+    //Array
+    $names = array(Johnny","Kristan","Cameron");
+    echo $names['2'];
+    ?>
+    
+    </body>
+</html>
