1cfc86e5f7c59dd913333b0ca455771153253d07
Kristan-coder <141744270+Kristan-coder@users.noreply.github.com>
2025-04-02 09:39:07 -0500

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -4,8 +4,48 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/DataTypes.php">Datatypes</a></li>
+            <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/operators.php">Operators</a></li>
+            <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/conditionals.php">Conditionals</a></li>
+            <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/calculator.php"> Calculator</a></li>
+            <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/calendar.php"> Calendar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
   <form method="GET">
 <input type="text" name="num1" placeholder="Number 1">
@@ -29,20 +69,20 @@ if (isset($_GET['submit'])) {
   $operator = $_GET['operator'];
   switch ($operator) {
     case "None":
-      echo "SELECT A METHOD!!!";
-      break;
-      case "Add":
-        echo $result1 + $result2;
-        break;
-        case "Subtract":
-          echo $result1 - $result2;
-          break;
-          case "Multiply":
-            echo $result1 * $result2;
-            break;
-            case "Divide":
-              echo $result1 /  $result2;
-              break;
+    echo "SELECT A METHOD!!!";
+    break;
+    case "Add":
+    echo $result1 + $result2;
+    break;
+    case "Subtract":
+    echo $result1 - $result2;
+    break;
+    case "Multiply":
+    echo $result1 * $result2;
+    break;
+    case "Divide":
+    echo $result1 /  $result2;
+    break;
   }
 }
 ?>
