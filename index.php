e5d8414d6920dabdca29415cfa91fac2c9d00aa2
Kristan-coder <141744270+Kristan-coder@users.noreply.github.com>
2025-04-08 09:44:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="style.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   </head>
   <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -16,7 +16,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="http://localhost/PHP-Lessons/index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
@@ -30,6 +30,7 @@
             <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/operators.php">Operators</a></li>
             <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/conditionals.php">Conditionals</a></li>
             <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/calculator.php"> Calculator</a></li>
+            <li><a class="dropdown-item" href="http://localhost/PHP-Lessons/calendar.php"> Calendar</a></li>
             <li><hr class="dropdown-divider"></li>
             
           </ul>
@@ -50,12 +51,14 @@
     <button>SUBMIT</button>
 </form>
     <?php
-    $name = $_GET['person'];
+    $name = $_GET ['person'];
     echo $name. " is bad at growing hair";
     //This gets something from the database
    
     /*echo "hi";*/
     
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js"></script>
   </body>
 </html>`
